5ce71c045a052bba65e05f6c2a0e9953d809d3e9
Claudio Benvenuti <claudio.benvenuti@gmail.com>
2026-02-04 14:47:31 +0100

    persist only SSH host keys, not entire /etc/ssh directory

--- modules/impermanence.nix
+++ modules/impermanence.nix
@@ -62,13 +62,18 @@ in
     hideMounts = true;
     directories = [
       "/etc/nixos"
-      "/etc/ssh"
       {
         directory = "/home/informatica";
         user = "informatica";
         group = "users";
         mode = "0755";
       }
+    ];
+    files = [
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/etc/ssh/ssh_host_ed25519_key.pub"
+      "/etc/ssh/ssh_host_rsa_key"
+      "/etc/ssh/ssh_host_rsa_key.pub"
     ];
   };
 }
