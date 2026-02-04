12b562404d60ffd4601280289202bb5dee1124de
Claudio Benvenuti <claudio.benvenuti@gmail.com>
2026-02-04 21:46:26 +0100

    enable nix-command and flakes experimental features

--- modules/common.nix
+++ modules/common.nix
@@ -1,6 +1,9 @@
 { config, pkgs, lib, ... }:
 
 {
+  # Enable flakes and nix-command
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # Bootloader.
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
