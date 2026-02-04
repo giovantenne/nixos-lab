4ef3dc1f54cf8fe5ba917aba9e20fdb39befd146
Claudio Benvenuti <claudio.benvenuti@gmail.com>
2026-02-04 15:54:11 +0100

    use XDG standard directories in home template

--- assets/create-home-template.sh
+++ assets/create-home-template.sh
@@ -17,9 +17,17 @@ cat > "$TEMPLATE_DIR/.gitconfig" << EOF
     email = $GIT_EMAIL
 EOF
 
-# Create basic directories
-mkdir -p "$TEMPLATE_DIR/Documenti"
-mkdir -p "$TEMPLATE_DIR/Progetti"
+# Create XDG standard directories
+mkdir -p "$TEMPLATE_DIR/Desktop"
+mkdir -p "$TEMPLATE_DIR/Documents"
+mkdir -p "$TEMPLATE_DIR/Downloads"
+mkdir -p "$TEMPLATE_DIR/Music"
+mkdir -p "$TEMPLATE_DIR/Pictures"
+mkdir -p "$TEMPLATE_DIR/Public"
+mkdir -p "$TEMPLATE_DIR/Templates"
+mkdir -p "$TEMPLATE_DIR/Videos"
+
+# Create config directories
 mkdir -p "$TEMPLATE_DIR/.config"
 mkdir -p "$TEMPLATE_DIR/.local/share"
 
