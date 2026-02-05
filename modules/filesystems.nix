0dd90ab18bb55111b0012002da7bb7624bd5b1bc
Claudio Benvenuti <claudio.benvenuti@gmail.com>
2026-02-05 17:10:29 +0100

    Remove duplicate fileSystems declarations (disko handles them)

--- modules/filesystems.nix
+++ modules/filesystems.nix
@@ -1,27 +1,6 @@
 { ... }:
-let
-  btrfsDevice = "/dev/disk/by-label/nixos";
-  btrfsOptions = [ "compress=zstd" "noatime" ];
-in
 {
+  # Btrfs support (filesystems are declared by disko)
   boot.supportedFilesystems = [ "btrfs" ];
   boot.initrd.supportedFilesystems = [ "btrfs" ];
-
-  fileSystems."/" = {
-    device = btrfsDevice;
-    fsType = "btrfs";
-    options = [ "subvol=@root" ] ++ btrfsOptions;
-  };
-
-  fileSystems."/home/informatica" = {
-    device = btrfsDevice;
-    fsType = "btrfs";
-    options = [ "subvol=@home-informatica" ] ++ btrfsOptions;
-  };
-
-  fileSystems."/var/lib/home-snapshots" = {
-    device = btrfsDevice;
-    fsType = "btrfs";
-    options = [ "subvol=@snapshots" ] ++ btrfsOptions;
-  };
 }
