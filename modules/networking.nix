4a0624a23d149da3fca9ad4c369acc9ef903b25f
Claudio Benvenuti <claudio.benvenuti@gmail.com>
2026-02-06 13:45:08 +0100

    Fix iface attribute duplication in networking module

--- modules/networking.nix
+++ modules/networking.nix
@@ -1,13 +1,15 @@
 { hostIp, hostName, labSettings, ... }:
 {
   networking.hostName = hostName;
-  networking.interfaces.${labSettings.ifaceName} = {
-    useDHCP = true;
+  networking.interfaces = {
+    ${labSettings.ifaceName} = {
+      useDHCP = true;
+      ipv4.addresses = [
+        {
+          address = hostIp;
+          prefixLength = 24;
+        }
+      ];
+    };
   };
-  networking.interfaces.${labSettings.ifaceName}.ipv4.addresses = [
-    {
-      address = hostIp;
-      prefixLength = 24;
-    }
-  ];
 }
