1e6dd927582019f0108d31e8610b6292d5e5313c
Claudio Benvenuti <claudio.benvenuti@gmail.com>
2026-02-06 13:15:57 +0100

    change user order

--- modules/users.nix
+++ modules/users.nix
@@ -7,14 +7,11 @@
   # Disable root login
   users.users.root.hashedPassword = "!";
 
-  users.users.admin = {
+  users.users.docente = {
     isNormalUser = true;
-    description = "admin";
-    extraGroups = [ "networkmanager" "wheel" "docker" ];
-    hashedPassword = "$6$ARnkNr/aUtpwcxD4$kogxkOwkeqvwtsd0WXyQ3IhqXdLQgSXyjHV3Jb2GtPpHQWm/epABQvMTKmAu9MoVeLO5NTQCsjYwmadyumDas.";
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAeU4p5Nv6Ak8AJp4GiTlWHJUzuOEOuv5C2Am5mRnffV admin@pc31"
-    ];
+    description = "docente";
+    extraGroups = [ "networkmanager" "docker" ];
+    hashedPassword = "$6$6g9aF3VchadHSTHG$wry4cmIljUGHB4SamaWB7ZeTtjbrBmRpP323AX5Jw3xMkRn4N.is6cH3J/0XgE8Xk01FhzwMLtA4KISsVMAbK.";
   };
 
   users.users.informatica = {
@@ -24,11 +21,14 @@
     hashedPassword = "$6$d7Y6egRmcsYHzkJE$sODDV60wD7qra8HAKgzAIOk2/EMMTqpb7LW2rbkvp/FNu9muJZeQT0FIbipUesftWrnGPlszKyKGmNtVdEbVs1";
   };
 
-  users.users.docente = {
+  users.users.admin = {
     isNormalUser = true;
-    description = "docente";
-    extraGroups = [ "networkmanager" "docker" ];
-    hashedPassword = "$6$6g9aF3VchadHSTHG$wry4cmIljUGHB4SamaWB7ZeTtjbrBmRpP323AX5Jw3xMkRn4N.is6cH3J/0XgE8Xk01FhzwMLtA4KISsVMAbK.";
+    description = "admin";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    hashedPassword = "$6$ARnkNr/aUtpwcxD4$kogxkOwkeqvwtsd0WXyQ3IhqXdLQgSXyjHV3Jb2GtPpHQWm/epABQvMTKmAu9MoVeLO5NTQCsjYwmadyumDas.";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAeU4p5Nv6Ak8AJp4GiTlWHJUzuOEOuv5C2Am5mRnffV admin@pc31"
+    ];
   };
 
   services.displayManager.autoLogin = {
