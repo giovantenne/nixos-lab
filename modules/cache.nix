01082bd7e2cdd5ffea37eec14cf5ddfd55b6568d
Claudio Benvenuti <claudio.benvenuti@gmail.com>
2026-02-09 22:54:56 +0100

    fix: skip self-referencing cache on master

--- modules/cache.nix
+++ modules/cache.nix
@@ -1,7 +1,11 @@
-{ labSettings, ... }:
+{ labSettings, hostName, ... }:
+let
+  isMaster = hostName == labSettings.masterHostName;
+in
 {
   nix.settings = {
-    substituters = [ "http://${labSettings.masterIp}:${toString labSettings.cachePort}" ];
+    # The master does not need itself as a substituter
+    substituters = if isMaster then [] else [ "http://${labSettings.masterIp}:${toString labSettings.cachePort}" ];
     trusted-public-keys = [ labSettings.cachePublicKey ];
   };
 }
