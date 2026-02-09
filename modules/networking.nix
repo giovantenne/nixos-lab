c513416500f2e25125384d2259522f08448f4192
Claudio Benvenuti <claudio.benvenuti@gmail.com>
2026-02-09 21:53:00 +0100

    fix: disable networkmanager on lab iface

--- modules/networking.nix
+++ modules/networking.nix
@@ -1,21 +1,16 @@
 { hostIp, hostName, labSettings, ... }:
 {
   networking.hostName = hostName;
-  networking.networkmanager.ensureProfiles = {
-    lab = {
-      connection = {
-        id = "lab";
-        type = "ethernet";
-        interface-name = labSettings.ifaceName;
-        autoconnect = true;
-      };
-      ipv4 = {
-        method = "auto";
-        addresses = [ "${hostIp}/24" ];
-      };
-      ipv6 = {
-        method = "auto";
-      };
+  networking.networkmanager.unmanaged = [ "interface-name:${labSettings.ifaceName}" ];
+  networking.interfaces = {
+    ${labSettings.ifaceName} = {
+      useDHCP = true;
+      ipv4.addresses = [
+        {
+          address = hostIp;
+          prefixLength = 24;
+        }
+      ];
     };
   };
 }
